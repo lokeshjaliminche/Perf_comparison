c8cc79772ca1a1132fa2f37517c0d2a87de1c1a6
Lokesh Jaliminche <lokesh.jaliminche@gmail.com>
2020-05-31 15:46:53 -0700

    Added sweeping scripts

---
+++ sweeping_scripts/png/filter_png_2000.py
@@ -0,0 +1,11 @@
+import cv2 
+import numpy as np 
+
+src=/home/ljaliminche/Desktop/CSE220/project/Latest/images_png
+dst=/home/ljaliminche/Desktop/CSE220/project/Latest/result
+
+for x in range(1, 2000):
+    img = cv2.imread(str(src) + str(x) +".png") 
+    sobelx = cv2.Sobel(img[:,:,0],cv2.CV_64F,1,0,ksize=5) 
+    #io.imshow(edges)
+    cv2.imwrite(str(dst) + str(x) +".png",sobelx)
---
+++ sweeping_scripts/run.sh
@@ -0,0 +1,21 @@
+
+
+#scripts for jpg images
+
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_jpg_500.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/jpg/filter_jpg_500.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_jpg_1000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/jpg/filter_jpg_1000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_jpg_2000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/jpg/filter_jpg_2000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_jpg_3000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/jpg/filter_jpg_3000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_jpg_4000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/jpg/filter_jpg_4000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_jpg_5000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/jpg/filter_jpg_5000.py
+
+
+
+#scripts for png images
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_png_500.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/png/filter_png_500.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_png_1000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/png/filter_png_1000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_png_2000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/png/filter_png_2000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_png_3000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/png/filter_png_3000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_png_4000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/png/filter_png_4000.py
+sudo perf stat -d -d -o /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/result_log/filter_png_5000.log python3 /home/ljaliminche/Desktop/CSE220/project/Latest/sweeping_scripts/png/filter_png_5000.py
+
